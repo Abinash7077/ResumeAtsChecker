37f4a937d34c143d5d9119b5aec922fddabe86c0
Abinash7077 <abinashmohanty227@gmail.com>
2025-10-01 19:25:28 +0530

    udapte vercel url

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:8000",
         "https://resumeatschecker-1-eeh8.onrender.com",
-        "https://ats-resume-frontend-phi.vercel.app/",
+        "https://ats-resume-frontend-phi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
