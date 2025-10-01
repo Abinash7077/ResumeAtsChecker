b2b39b40e11b846f885b5b0048c503ba17746415
Abinash7077 <abinashmohanty227@gmail.com>
2025-10-01 10:00:30 +0530

    Added cors url

--- src/main.py
+++ src/main.py
@@ -26,7 +26,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+   allow_origins=[
+        "http://localhost",
+        "http://localhost:3000",
+        "http://127.0.0.1:8000",
+        "https://resumeatschecker-1-eeh8.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
